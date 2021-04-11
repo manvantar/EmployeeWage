987e9207e8186d5f8f676c2ab3f5f02b16c60122
manvantar <manukvshetty@gmail.com>
2021-04-11 12:30:17 +0530

    Displaying welcom message

---
+++ EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day7;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage problem");
+    }
+
+}
