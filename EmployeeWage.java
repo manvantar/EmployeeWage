c5c4eb2c443d8d6869f1294b43c8ab6d58c37af8
manvantar <manukvshetty@gmail.com>
2021-04-11 13:06:19 +0530

    Solving using swithcase and calculating the Wage per day

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,10 +9,27 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage problem");
         Random rand=new Random();
-        int attendance=rand.nextInt(10)%2;
-        int fullTimeHours=8;
-        int partTimeHours=8;
-        int oneDayWage= wagePerHour*fullTimeHours;
+        int typeOfEmployee=rand.nextInt(10)%3;
+        int hours=0;
+        switch (typeOfEmployee){
+            case 2:
+                System.out.println("FullTimer");
+                hours=8;
+                break;
+            case 1:
+                System.out.println("PartTimer");
+                hours=8;
+                break;
+            default:
+                System.out.println("Absent");
+                hours=0;
+                break;
+        }
+
+
+
+        int oneDayWage= wagePerHour*hours;
+        System.out.println(oneDayWage);
 
 
     }
